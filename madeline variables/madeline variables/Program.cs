87e3b569f5901301781835760b61e1e56ee1b423
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-13 14:38:04 -0300

    updated variables assignment

--- madeline variables/madeline variables/Program.cs
+++ madeline variables/madeline variables/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace madeline_variables
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("variables");
+            Console.WriteLine();
+            Console.WriteLine();
+
+                int score; // this line declares a variable as an interger named score
+            int enemyvalue;
+            string realName;
+            String GamerTag;
+            float percentile;
+
+            // setting up values
+            score = 0;
+            enemyvalue = 250;
+            score = score + 10;  // because the enemy value is set to 250 using the variable enemyvalue plus score is essentially the same as score plus that number
+            realName = "maddy";
+            GamerTag = "gamer";
+            percentile = 85.543f; 
+
+            //pretend gameplay: killed an enemy
+            score = score + enemyvalue; // = means that it gets the value of something, aka saying this is this.
+
+            
+            //hud
+            Console.WriteLine (realName + "(" + GamerTag + ") ");
+            // dialog
+            Console.WriteLine();
+           // Console.WriteLine("yo,")
+           
+                      
+
+            Console.WriteLine(score);
+
+            Console.WriteLine();
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
