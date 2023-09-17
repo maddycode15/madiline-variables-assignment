90501bf83729c3cdf316b130441a6ed9bc158127
matthew allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-17 20:31:49 -0300

    sunday update

--- madeline variables/madeline variables/Program.cs
+++ madeline variables/madeline variables/Program.cs
@@ -17,31 +17,35 @@ namespace madeline_variables
 
                 int score; // this line declares a variable as an interger named score
             int enemyvalue;
+            int HP;
             string realName;
             String GamerTag;
             float percentile;
 
             // setting up values
+           HP  = 100;
             score = 0;
             enemyvalue = 250;
-            score = score + 10;  // because the enemy value is set to 250 using the variable enemyvalue plus score is essentially the same as score plus that number
-            realName = "maddy";
-            GamerTag = "gamer";
+           // score = score + 10;  // because the enemy value is set to 250 using the variable enemyvalue plus score is essentially the same as score plus that number
+            realName = "Adventure Quest";
+            GamerTag = "pixle studios";
             percentile = 85.543f; 
 
             //pretend gameplay: killed an enemy
-            score = score + enemyvalue; // = means that it gets the value of something, aka saying this is this.
+           // score = score + enemyvalue; // = means that it gets the value of something, aka saying this is this.
 
             
             //hud
             Console.WriteLine (realName + "(" + GamerTag + ") ");
             // dialog
-            Console.WriteLine();
-           // Console.WriteLine("yo,")
-           
-                      
+            Console.WriteLine("hey");
+            // Console.WriteLine("yo,")
+
+            Console.WriteLine("health"); Console.WriteLine(HP);
 
             Console.WriteLine(score);
+
+            Console.WriteLine("enemy uses slash");
 
             Console.WriteLine();
             Console.WriteLine("press any key to exit...");
