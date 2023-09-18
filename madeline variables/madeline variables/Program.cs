fbc04c12ab573a82f4c2e6b5a17e1b40ce2a4770
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-18 09:54:52 -0300

    updated game

--- madeline variables/madeline variables/Program.cs
+++ madeline variables/madeline variables/Program.cs
@@ -18,17 +18,25 @@ namespace madeline_variables
                 int score; // this line declares a variable as an interger named score
             int enemyvalue;
             int HP;
+            int Slash;
+            int Enemy_Value;
+            int Enemy_Health;
+            int Punch;
             string realName;
-            String GamerTag;
+            string GameName;
+            String Studio;
             float percentile;
 
             // setting up values
            HP  = 100;
             score = 0;
             enemyvalue = 250;
+            Slash = -20;
+            Enemy_Health = 20;
+            realName = "player";
            // score = score + 10;  // because the enemy value is set to 250 using the variable enemyvalue plus score is essentially the same as score plus that number
-            realName = "Adventure Quest";
-            GamerTag = "pixle studios";
+            GameName = "Adventure Quest";
+            Studio = "pixle studios";
             percentile = 85.543f; 
 
             //pretend gameplay: killed an enemy
@@ -36,16 +44,28 @@ namespace madeline_variables
 
             
             //hud
-            Console.WriteLine (realName + "(" + GamerTag + ") ");
+            Console.WriteLine (GameName + "(" + Studio + ") ");
             // dialog
-            Console.WriteLine("hey");
+            //Console.WriteLine("hey");
             // Console.WriteLine("yo,")
 
-            Console.WriteLine("health"); Console.WriteLine(HP);
 
-            Console.WriteLine(score);
 
+            Console.WriteLine("health" + " " + HP + " ");
+
+            Console.WriteLine("score" + ' ' + score); 
+ 
             Console.WriteLine("enemy uses slash");
+
+            //Console.WriteLine ("health" + HP - Slash); this is not how it works. you need to do the equation that removes the slash damage from the hp and then write the new value of health.
+
+            HP = HP + Slash;
+
+            Console.WriteLine("health" + " " + HP + " ");
+
+            Console.WriteLine("score" + ' ' + score);
+
+
 
             Console.WriteLine();
             Console.WriteLine("press any key to exit...");
